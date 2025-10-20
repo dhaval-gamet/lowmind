30643945e166b115e096666582254b173732eaf1
VEDRA <gametidhaval980@gmail.com>
2025-10-20 06:00:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="lowmind",
-    version="0.2.0",
-    packages=find_packages(),
+    version="0.3.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+
     install_requires=[
         "numpy>=1.19.0",
         "psutil>=5.8.0"
     ],
     author="VEDRA",
     description="Ultra-lightweight Deep Learning Framework for Raspberry Pi",
-    long_description=open("README.md").read(),
+    long_description=open("README .md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/dhaval-gamet/lowmind",
     classifiers=[
